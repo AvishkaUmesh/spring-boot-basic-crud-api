b3e0fbc056954e4ef8004690ae2a7f86ae44d23e
Avishka Umesh <umesh.avishka@gmail.com>
2023-05-07 11:20:09 +0530

    first commit

---
+++ src/main/java/com/avishka/springbootbasiccrudapi/SpringBootBasicCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.avishka.springbootbasiccrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avishka/springbootbasiccrudapi/SpringBootBasicCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avishka.springbootbasiccrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
